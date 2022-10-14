cebcf6ccdc8ac1ee17a56958590db6d9ac6cc535
Ying <ychua@hubspot.com>
2022-10-14 17:03:52 +0800

    LAUNCH

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const https = require('https');
 const cookieParser = require('cookie-parser');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
@@ -10,28 +10,27 @@ const CONNECTION_URL = "mongodb+srv://ychua:cXPF5lDHxmvNo0ul@nanostacko.ahrgsi7.
 const DATABASE_NAME = "nanostacko"; // you can change the database name
 var database, collection;
 
-app.use(cookieParser());
-
-app.use(express.urlencoded({extended: true}));
-app.get("/", function(req, res){
-  res.render('home');
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+  if(error) throw error;
+  database = client.db(DATABASE_NAME);
+  collection = database.collection("newcollection"); // you can change the collection name
 });
-
-app.set("view engine", "ejs");
-
-app.post("/", function(req, res){
-  collection.insertOne(req.body, (err, result) => {  
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+app.use('/css', express.static('/Users/acrestois/Documents/code-things/node_modules/bootstrap/dist/css'));
+app.set("view engine","ejs");
+app.get("/", function(req, res){
+    res.render('home');
+  });
+app.post("/", (req, res) =>{
+  collection.insertOne(req.body, (err, result) => {
     if (err) return console.log(err)
-
     console.log('saved to database')
-    res.redirect('/') // or do something else here
   })
-
   function formv3(){
-    // Create the new request 
+    // Create the new request
     var xhr = new XMLHttpRequest();
-    var url = 'https://api.hsforms.com/submissions/v3/integration/submit/7177581/2ec46888-7cf7-4d58-9cee-9fcaca397feb'
-    
+    var url = 'https://api.hsforms.com/submissions/v3/integration/submit/9381732/174db96c-c8b2-4041-a6f7-60cd29fd5e87'
     // Example request JSON:
     var data = {
       "fields": [
@@ -50,45 +49,31 @@ app.post("/", function(req, res){
         "pageName": "Portfolio contact me"
       }
     }
-
     var final_data = JSON.stringify(data)
-
+console.log(final_data);
     xhr.open('POST', url);
     // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
     xhr.setRequestHeader('Content-Type', 'application/json');
-
     xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4 && xhr.status == 200) { 
+        if(xhr.readyState == 4 && xhr.status == 200) {
             console.log(xhr.responseText); // Returns a 200 response if the submission is successful.
-        } else if (xhr.readyState == 4 && xhr.status == 400){ 
-            console.log(xhr.responseText); // Returns a 400 error the submission is rejected.          
-        } else if (xhr.readyState == 4 && xhr.status == 403){ 
-            console.log(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
-        } else if (xhr.readyState == 4 && xhr.status == 404){ 
-            console.log(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
+        } else if (xhr.readyState == 4 && xhr.status == 400){
+            console.log(xhr.responseText); // Returns a 400 error the submission is rejected.         
+        } else if (xhr.readyState == 4 && xhr.status == 403){
+            console.log(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.          
+        } else if (xhr.readyState == 4 && xhr.status == 404){
+            console.log(xhr.responseText); //Returns a 404 error if the formGuid isn't found    
         }
        }
-
-
-    // Sends the request 
-    
+    // Sends the request
     xhr.send(final_data)
+    console.log('xhr runs');
  }
-
  formv3();
-});
-
-
-MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-  if(error) throw error;
-
-  database = client.db(DATABASE_NAME);
-  collection = database.collection("newcollection"); // you can change the collection name
-
+ console.log('formv3 has executed?');
+ res.redirect("/") // or do something else here
+})
   // Start the application after the database connection is ready
   app.listen(3000, () => {
     console.log('This app is running on port 3000')
   });
-});
-
-
